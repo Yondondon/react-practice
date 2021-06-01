54d765b2d817f9d1109b525403603f835590b775
Yondondon <roman17091993@gmail.com>
2021-06-02 01:31:27 +0300

    removed some trash

--- src/components/Projects/App.js
+++ src/components/Projects/App.js
@@ -9,9 +9,7 @@ class Projects extends Component {
     return(
       <div className="main_wrapper">
         <div className="main_container">
-          <Link className="link_to_app" to={'/counter'}>Counter</Link>
           <Link className="link_to_app" to={'/todo'}>Todo</Link>
-          <Link className="link_to_app" to={'/my_anime_list'}>My Anime List</Link>
         </div>
       </div>
     )
--- src/index.js
+++ src/index.js
@@ -10,12 +10,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 //components
 import MainApp from './components/MainApp';
 import TodoApp from './components/Todo/App';
-import Counter from './components/Counter/App';
-import MyAnimeList from './components/MyAnimeList/App';
 import Projects from './components/Projects/App';
-import Login from './components/Login/App';
-import PrivateRoute from './components/PrivateRoute/App';
-import PublicRoute from './components/PublicRoute/App';
 
 
 const store = createStore(
@@ -28,11 +23,8 @@ ReactDOM.render(
     <BrowserRouter>
       <Switch>
         <Route exact path='/' component={MainApp} />
-        {/* <PublicRoute restricted={true} path='/login' component={Login} /> */}
-        <PublicRoute path='/todo' component={TodoApp} />
-        <PublicRoute path='/counter' component={Counter} />
-        <PublicRoute path='/my_anime_list' component={MyAnimeList} />
-        <PublicRoute path='/projects' component={Projects} />
+        <Route path='/todo' component={TodoApp} />
+        <Route path='/projects' component={Projects} />
       </Switch>
     </BrowserRouter>
   </Provider>,
